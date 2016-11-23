4a2bdc22d24f4836a507479cbc3fd14e1cfb8408
amonik <adeyinamon@yahoo.com>
2016-11-23 01:36:30 -0600

    read in code and plot time and freq

--- sample.m
+++ sample.m
@@ -1,1 +1,15 @@
-filename = "/home
+filename = 'AnitWhatYouDoByJimmieLunceford.mp3';
+[signal,fs] = audioread(filename,'native');
+t = linspace(0, length(signal)/fs, length(signal));
+figure
+plot(t,signal)
+xlabel('time');
+x = 14;
+NFFT = 2^x;
+f = linspace(0,fs,NFFT);
+samples = abs(fft(signal,NFFT));
+figure
+plot(f(1:NFFT/2),samples(1:NFFT/2))
+xlabel('Frequecy');
+
+
